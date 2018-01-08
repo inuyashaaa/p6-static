ca0d675addc3a8202294336e22e88525f75cbdee
inuyashaaa <huymanhtmhp@gmail.com>
2018-01-09 03:12:24 +0700

    Upload images with multer

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const _ = require('lodash');
 
+const mkdirp = require('mkdirp');
+const path = require('path');
+const multer = require('multer');
+
 const app = express();
 
 // Routes
@@ -10,6 +14,42 @@ app.get('/', (req, res) =>
   res.json(
     _.pick(packageJson, ['name', 'version', 'description', 'author', 'license'])
   )
+);
+
+const allowTypes = process.env.ALLOW_TYPES.split(',').map(data => data.trim());
+
+const uploadConfig = {
+  fields: process.env.MAX_FIELD || 17,
+  files: process.env.MAX_FILE || 17,
+  fileSize: (process.env.MAX_SIZE || 100) * 1048576,
+  parts: process.env.MAX_PART || 17
+};
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    const today = new Date();
+    const dd = today.getDate();
+    const mm = today.getMonth() + 1; // January is 0!
+    const yyyy = today.getFullYear();
+    const uploadPath = `public/resource/${yyyy}/${mm}/${dd}`;
+    mkdirp(`${uploadPath}`);
+    cb(null, `${path.resolve(__dirname, uploadPath)}`);
+  },
+  filename(req, { originalname, mimetype }, cb) {
+    const nameSegments = originalname.split('.');
+    const name = nameSegments[0] || `${Date.now()}`;
+
+    const mineTypeSegments = mimetype.split('/');
+    const ext = mineTypeSegments[1] || 'jpeg';
+    cb(null, `${Date.now()}-${name}.${ext}`);
+  }
+});
+const fileFilter = (req, { mimetype }, cb) =>
+  cb(null, Boolean(allowTypes.indexOf(mimetype) > -1));
+const uploader = multer({ storage, fileFilter, limits: uploadConfig });
+
+app.post('/upload', uploader.array('images'), (req, res) =>
+  res.json({ images: req.files })
 );
 
 const port = process.env.PORT || 9999;
